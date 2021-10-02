1be5db954794fa9f7087b6e28ac689ff4add1260
Light <47911535+LightAPIs@users.noreply.github.com>
2021-10-02 20:16:15 +0800

    perf(background): 排除掉从其他目录复制过来的情况

--- src/background.js
+++ src/background.js
@@ -6,10 +6,11 @@ chrome.bookmarks.onCreated.addListener((id, bookmark) => {
     chrome.bookmarks.getChildren(parentId, results => {
       if (!chrome.runtime.lastError) {
         for (const children of results) {
-          if (title.includes(children.title)) {
+          if (title.includes(children.title) && title !== children.title) {
             chrome.bookmarks.update(id, {
               title: title.replace(/\s*\(\d+\)$/, ''),
             });
+            break;
           }
         }
       }
